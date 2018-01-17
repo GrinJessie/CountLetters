1061f21ff5ca4794586323d9b0516f996fcb2f0e
Lijing Wang <lijing@ualberta.ca>
2018-01-17 20:54:42 +0000

    wrote and debuged the function

--- countLetters.js
+++ countLetters.js
@@ -1,5 +1,24 @@
-//function
-//param: string
-//return object
-//unique char in a string
-//# instances of each char
+//input: Str
+//Str: no spaces, loweercase
+//define object: Letters
+//for char in Str:
+//if !Letters[letter] true
+//Letters[letter] = 1;
+//else Letters[letter] += 1;
+//return
+
+
+var countLetters = function(str){
+  var strs = str.toLowerCase().split('');
+  var letters = {};
+  for (let char of strs){
+    if (!letters[char]) {
+      letters[char] = 1;
+    } else {
+      letters[char] += 1;
+    }
+  }
+  return letters;
+};
+
+console.log(countLetters('lighthouse in the house'));
